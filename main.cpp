d407b863581b887d18469a0cef36f194e78c39d2
pricelessArtist <priceboom811@gmail.com>
2026-03-04 01:20:51 -0500

    Updating main.cpp code

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ int main() {
     list.insertNode('1');
     list.insertNodeAfter('2', '1');
     list.insertNodeAfter('3', '2');
+    list.insertNodeAfter('4', '3');
+    list.insertNodeAfter('5', '4');
+    list.insertNodeAfter('6', '5');
+    list.insertNodeAfter('7', '6');
+    list.insertNodeAfter('8', '7');
+    list.insertNodeAfter('9', '8');
     list.iterateForward();
     list.iterateBackwards();
     return 0;
